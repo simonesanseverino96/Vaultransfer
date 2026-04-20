1acbacde857e86dca9c0aac0f702a419807d9bca
simone sanseverino <simonesanseverino45@gmail.com>
2026-04-20 13:08:09 +0200

    Fix robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,13 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://vaultransfer.it'
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://vaultransfer.com'
 
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/'],
+      disallow: ['/api/', '/download/'],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   }
